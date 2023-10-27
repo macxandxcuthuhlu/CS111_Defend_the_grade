b1543d0876448f6c18ed16b7356cbfc5fea0b28d
itsasam1 <sstuc2@uic.edu>
2023-10-26 22:25:22 -0500

    Just worked on a little practice

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,7 +1,35 @@
-def user_input(lista):
-    for i in lista:
-        lista.append(i)
+
+def list_to_int(list):
+    list1 = []
+    for i in range(len(list)):
+        list1.append(int(list[i]))
+    list = list1
+    return list
         
-listm = ['a', 'b', 'x', 'd']
-user_input(listm)
-print(listm)
+
+lista = ['1', '12', '22', '3', '3']
+lista = list_to_int(lista)
+print(lista)
+m = sum(lista)
+print(m)
+
+
+def mystery1(inp):
+    out = 0
+    for i in range (len(inp)):
+        out = out + inp[i]
+    return out
+
+d = mystery1(lista)
+print(d)
+
+def buggy_mystery(list):
+    last = list[-1]
+    for i in range(len(list)-1):
+        list[i] = list[i]/last
+    
+buggy_mystery(lista)
+print(lista)
+
+d = lista[:]
+print(d)
